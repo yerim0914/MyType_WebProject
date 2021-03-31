528a95d3f86f3fb4fc8c67ccf65210cf4ac4ad49
unknown <morri0914@hanmail.net>
2021-04-01 00:58:33 +0900

    'two'

---
+++ js/form.js
@@ -0,0 +1,27 @@
+function scrollup(){
+    const vheight = $('.test').height(); /* 폼 높이 가져오기 */
+    $('html, body').animate({
+        scrollTop: ((Math.floor($(window.scrollTop)))
+        )
+    })
+}
+
+function scrolldown(){
+
+
+}
+
+$(function(){
+    $('.next_btn').click(function(e)){
+
+    }
+    $('.prev_btn').click(function(e)){
+        
+    }
+    $('#form').click(function(e)){
+        
+    }
+    $('html, body').animate({
+        
+    }
+});
